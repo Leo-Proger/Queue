87880bd614431a53f3489e003ee5cb305f43072c
Leo Proger <leonidkrasavin4@gmail.com>
2024-05-06 20:32:23 +0300

    Создана кольцевая очередь чисел

---
+++ src/main/java/IntegerQueue/CircularQueue/Queue.java
@@ -0,0 +1,39 @@
+package IntegerQueue.CircularQueue;
+
+import IntegerQueue.IntegerQueue;
+
+import java.util.NoSuchElementException;
+
+public class Queue implements IntegerQueue {
+    public final int size;
+    private final int[] queue;
+    private int putLocation, getLocation;
+
+    // Создает очередь с заданным размером
+    public Queue(int size) {
+        this.size = size;
+        queue = new int[size + 1];
+        putLocation = getLocation = 0;
+    }
+
+    // Добавить символ в очередь
+    public void put(int value) {
+        /* Очередь переполнена, если putLocation на единицу меньше getLocation или
+        putLocation указывает на конец массива, а getLocation - на его начало */
+        if (putLocation + 1 == getLocation || ((putLocation == queue.length - 1) && (getLocation == 0))) {
+            throw new ArrayIndexOutOfBoundsException("Очередь переполнена");
+        }
+        queue[putLocation++] = value;
+        if (putLocation == queue.length) putLocation = 0; // Закольцевать
+    }
+
+    // Прочитать символ очереди
+    public int get() {
+        if (getLocation == putLocation) {
+            throw new NoSuchElementException("Очередь пуста");
+        }
+        int value = queue[getLocation++];
+        if (getLocation == queue.length) getLocation = 0; // Закольцевать
+        return value;
+    }
+}
