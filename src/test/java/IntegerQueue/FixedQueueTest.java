5b02158ac223d4168df190fe34314c48192471f1
Leo Proger <leonidkrasavin4@gmail.com>
2024-05-07 14:46:52 +0300

    Добавлены тесты для фиксированной очереди чисел. Добавлены зависимости для тестов в pom.xml

---
+++ src/test/java/IntegerQueue/FixedQueueTest.java
@@ -0,0 +1,94 @@
+package IntegerQueue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FixedQueueTest {
+
+    @Test
+    public void testCreateFixedQueue() {
+        FixedQueue queue = new FixedQueue(5);
+        assertNotNull(queue);
+        assertEquals(5, queue.size);
+        assertArrayEquals(new int[5], queue.getQueue());
+        assertEquals(0, queue.getPutLocation());
+        assertEquals(0, queue.getGetLocation());
+    }
+
+    @Test
+    public void testCreateFixedQueueWithNegativeSize() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new FixedQueue(-1));
+    }
+
+    @Test
+    public void testCreateFixedQueueWithZeroSize() {
+        FixedQueue queue = new FixedQueue(0);
+
+        assertNotNull(queue);
+        assertEquals(0, queue.size);
+        assertArrayEquals(new int[0], queue.getQueue());
+
+        assertEquals(0, queue.getPutLocation());
+        assertEquals(0, queue.getGetLocation());
+    }
+
+    @Test
+    public void testPutAndGet() {
+        FixedQueue queue = new FixedQueue(5);
+
+        queue.put(1);
+        queue.put(2);
+        queue.put(3);
+
+        assertArrayEquals(new int[]{1, 2, 3, 0, 0}, queue.getQueue());
+        assertEquals(3, queue.getPutLocation());
+        assertEquals(0, queue.getGetLocation());
+        assertEquals(3, queue.getCount());
+
+        assertEquals(1, queue.get());
+        assertEquals(2, queue.get());
+        assertEquals(3, queue.get());
+
+        assertArrayEquals(new int[]{0, 0, 0, 0, 0}, queue.getQueue());
+        assertEquals(3, queue.getPutLocation());
+        assertEquals(3, queue.getGetLocation());
+        assertEquals(0, queue.getCount());
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> queue.get());
+    }
+
+    @Test
+    public void testReset() {
+        FixedQueue queue = new FixedQueue(5);
+
+        queue.put(1);
+        queue.put(2);
+        queue.put(3);
+
+        queue.reset();
+
+        assertArrayEquals(new int[5], queue.getQueue());
+        assertEquals(0, queue.getPutLocation());
+        assertEquals(0, queue.getGetLocation());
+    }
+
+    @Test
+    public void testCopyConstructor() {
+        FixedQueue originalQueue = new FixedQueue(5);
+
+        originalQueue.put(1);
+        originalQueue.put(2);
+        originalQueue.put(3);
+
+        FixedQueue copyQueue = new FixedQueue(originalQueue);
+
+        assertNotSame(originalQueue, copyQueue);
+        assertArrayEquals(new int[]{1, 2, 3, 0, 0}, copyQueue.getQueue());
+        assertEquals(3, copyQueue.getPutLocation());
+        assertEquals(0, copyQueue.getGetLocation());
+    }
+}
